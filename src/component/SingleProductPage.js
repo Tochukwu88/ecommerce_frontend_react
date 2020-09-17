3933f4554ae40288ff3e8f1c39b5029d40cfe5df
tochukwu <teeceemum@gmail.com>
2020-09-17 18:04:29 +0100

    styled single product page

--- src/component/SingleProductPage.js
+++ src/component/SingleProductPage.js
@@ -2,10 +2,17 @@ import React, { useState, useEffect } from 'react'
 import Layout from './Layout'
 import { read, getRelatedProduct } from '../actions/product'
 import Card from './Card'
+import ShowPhoto from './ShowProductImage'
+import { addItem, getCart } from './cartHelpers'
+import { Link } from 'react-router-dom'
+import './SingleProduct.css'
+
+
  const SingleProductPage = (props) =>{
      const [product,setProduct] = useState({})
      const [relatedProduct,setRelatedProduct] = useState([])
      const [error,setError] = useState({})
+     const [inCart,setInCart] = useState(false)
      const LoadSingleProduct = productId =>{
          read(productId).then(data =>{
              if(data.error){
@@ -25,21 +32,67 @@ import Card from './Card'
          })
 
      }
+     const addtocart= () =>{
+        addItem(product,()=>{
+            // setRedirect(true)
+            console.log('added')
+
+        })
+    }
+    const checkCart = (productId) =>{
+        getCart().map((item,i)=>{
+            if(item._id === productId){
+                setInCart(true)
+            }
+        })
+     
+    }
+    const addToCartButton = () =>{
+        return  (
+         
+            <button className='card-btn btn ' onClick={addtocart}>
+                <Link className='card-link' to='/'>Add to cart</Link>
+            </button>
+        )
+
+
+    }
      useEffect(()=>{
-         const productId = props.match.params.productId
-         LoadSingleProduct(productId)
+        const productId = props.match.params.productId
+        checkCart(productId)
+      
+         
+         LoadSingleProduct(productId) 
      },[props])
      return(
          <>
              <Layout>
-                 <h1>product</h1>
-                {product && product.description &&  <Card product={product} showViewProductButton={false}/>}
-               <h1>related product</h1>
-               {relatedProduct.map((rp,i)=>{
-                   return(
-                    <Card key={i} product={rp}/>
-                   )
-               })}
+                
+                {product && product.description && (<div className='singleProduct-container'>
+                <div className='singleProduct-img'> <ShowPhoto item={product} url='product'/></div>
+               <div> <h1>{product.name}</h1>
+               <p className='price'>NGN{product.price}</p>
+                <p>{product.description}</p>
+                 {inCart?(<button className='card-btn btn '> <Link className='card-link' to='/cart'>go to cart</Link></button>):(addToCartButton())}
+                 
+                 
+                 
+                 </div>
+                </div>) }
+                
+              <div className='relatedP-container'>
+              <h2>related product</h2>
+              <div className='pbs-card'>
+              
+              {relatedProduct.map((rp,i)=>{
+                  return(
+                   <Card key={i} product={rp}/>
+                  )
+              })}
+              </div>
+
+              </div>
+            
               
 
              </Layout>
@@ -47,3 +100,4 @@ import Card from './Card'
      )
  }
  export default SingleProductPage 
+ //<Card product={product} showViewProductButton={false}/>
